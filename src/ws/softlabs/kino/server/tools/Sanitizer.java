49044191258cdcf92ce641b62920d8bb7a42cd14
Kami <kami@softlabs.ws>
2011-05-24 23:44:44 +1100

    Clean Log table during general clean procedure

--- src/ws/softlabs/kino/server/tools/Sanitizer.java
+++ src/ws/softlabs/kino/server/tools/Sanitizer.java
@@ -2,6 +2,8 @@ package ws.softlabs.kino.server.tools;
 
 import java.io.IOException;
 
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
+import ws.softlabs.lib.kino.dao.server.service.pmf.PMF;
 import ws.softlabs.lib.kino.dao.server.service.pmf.PMFModelDataService;
 
 @SuppressWarnings("serial")
@@ -22,6 +25,18 @@ public class Sanitizer extends HttpServlet {
 		log.debug("ENTER");
 		(new PMFModelDataService()).clearDatabase();
 		log.debug("CLEARED DATASTORE");
+		
+		PersistenceManager pm = PMF.getPersistenceManager();
+		try {
+			Query query = pm.newQuery(ScheduleLog.class);
+			query.deletePersistentAll();
+			log.debug("CLEARED LOG DATA");
+		} catch (Exception ex) {
+			log.debug("EXIT (EXCEPTION)" + ex);
+		} finally {
+			pm.close();
+		}
+		
 		log.debug("EXIT");
 	}
 }
